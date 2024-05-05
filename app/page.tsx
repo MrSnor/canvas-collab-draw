7d07ccde4eca91fd377714d0aa66085e8c7951b4
Partha Priyadarshi Das <66919291+MrSnor@users.noreply.github.com>
2024-05-05 12:25:42 +0530

    update info and basic logic files and add assets (including libs and utils)

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,3 @@
-import { Room } from './Room';
-import { CollaborativeApp } from './CollaborativeApp';
-
 export default function Page() {
-  return (
-    <Room>
-      {/* <CollaborativeApp /> */}
-      <h1 className='text-4xl'>Collborative Canvas App</h1>
-    </Room>
-  );
+  return <h1 className="text-4xl text-white">Collborative Canvas App</h1>;
 }
