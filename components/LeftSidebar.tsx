a5161c1731143eb9ea383c0e75cc76a66f1d94e9
Partha Priyadarshi Das <66919291+MrSnor@users.noreply.github.com>
2024-08-22 01:05:39 +0530

    update LeftSidebar.tsx UI

--- components/LeftSidebar.tsx
+++ components/LeftSidebar.tsx
@@ -18,14 +18,14 @@ const LeftSidebar = ({ allShapes }: { allShapes: Array<any> }) => {
             return (
               <div
                 key={shape[1]?.objectId}
-                className="group my-1 flex items-center gap-2 px-5 py-2.5 hover:cursor-pointer hover:bg-primary-green hover:text-primary-black"
+                className="group my-1 flex items-center gap-2 px-5 py-2.5"
               >
                 <Image
                   src={info?.icon}
                   alt='Layer'
                   width={16}
                   height={16}
-                  className='group-hover:invert'
+                  className=''
                 />
                 <h3 className='text-sm font-semibold capitalize'>{info.name}</h3>
               </div>
